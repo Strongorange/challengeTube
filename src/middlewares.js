0a2371b823275053c789a8ef923a9250a39c42f8
Strongorange <74127841+Strongorange@users.noreply.github.com>
2021-06-15 15:39:37 +0900

    Assignment 15 Start

--- src/middlewares.js
+++
@@ -1,6 +0,0 @@
-export const localsMiddleware = (req, res, next) => {
-  res.locals.siteTitle = "Nomad Users";
-  res.locals.loggedIn = req.session.loggedIn;
-  res.locals.loggedInUser = req.session.user;
-  next();
-};
