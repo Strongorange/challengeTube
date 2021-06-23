34c6099036890f5b94e0b47b86ed32800c43f952
Strongorange <74127841+Strongorange@users.noreply.github.com>
2021-06-23 09:17:02 +0900

    Assignment 18

--- src/index.js
+++ src/index.js
@@ -1,20 +1,105 @@
-import express from "express";
-import path from "path";
-import bodyParser from "body-parser";
-import multer from "multer";
-import fs from "fs";
-import morgan from "morgan";
-import rootRouter from "./routers/rootRouter";
+const video = document.querySelector("video");
+const videoController = document.getElementById("videoController");
+const psBtn = videoController.querySelector("#playPauseBtn");
+const volumeBtn = videoController.querySelector("#volume");
+const volumeRange = videoController.querySelector("#volumeRange");
+const currentTime = document.querySelector("#currentTime");
+const totalTime = document.querySelector("#totalTime");
+const timeline = document.getElementById("timeline");
+const fullScreenBtn = document.getElementById("fullScreen");
+const videoContainer = document.getElementById("videoContainer");
 
-const app = express();
-const PORT = 5300;
+let volumeValue = 0.5;
+video.volume = volumeValue;
 
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("dev"));
-app.use("/uploads", express.static("uploads"));
-app.use("/", rootRouter);
+const handlePlayAndStop = () => {
+  if (video.paused) {
+    video.play();
+    psBtn.className = "fas fa-pause";
+  } else {
+    video.pause();
+    psBtn.className = "fas fa-play";
+  }
+};
 
-// Codesanbox does not need PORT :)
-app.listen(PORT, () => console.log(`PORT 5300 Listening!`));
+const handleSound = () => {
+  if (video.muted) {
+    video.muted = false;
+    volumeRange.value = volumeValue;
+    volumeBtn.className = "fas fa-volume-up";
+  } else {
+    video.muted = true;
+    volumeRange.value = 0;
+    volumeBtn.className = "fas fa-volume-mute";
+  }
+};
+
+const handleVolume = (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    volumeBtn.className = "fas fa-volume-mute";
+  }
+  if (value === "0") {
+    volumeBtn.className = "fas fa-volume-off";
+  } else {
+    volumeBtn.className = "fas fa-volume-up";
+  }
+  video.volume = volumeValue = value;
+};
+
+const formatTime = (seconds) =>
+  new Date(seconds * 1000).toISOString().substr(11, 8);
+
+const handleLoadedMetadata = () => {
+  totalTime.innerText = formatTime(Math.floor(video.duration));
+  timeline.max = Math.floor(video.duration);
+};
+
+const handleTimeUpdate = () => {
+  currentTime.innerText = formatTime(Math.floor(video.currentTime));
+  timeline.value = Math.floor(video.currentTime);
+};
+
+const handleTimelineChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  video.currentTime = value;
+};
+
+const handleFullScreen = () => {
+  const fullscreen = document.fullscreenElement;
+  if (fullscreen) {
+    document.exitFullscreen();
+    fullScreenBtn.innerHTML = "Enter Full Screen";
+  } else {
+    videoContainer.requestFullscreen();
+    fullScreenBtn.innerHTML = "Exit Full Screen";
+  }
+  videoContainer.requestFullscreen();
+};
+
+const handleShortCut = (event) => {
+  const { code } = event;
+  console.log(code);
+  if (code === "KeyF") {
+    handleFullScreen();
+  } else if (code === "Space") {
+    handlePlayAndStop();
+  }
+};
+
+psBtn.addEventListener("click", handlePlayAndStop);
+volumeBtn.addEventListener("click", handleSound);
+volumeRange.addEventListener("input", handleVolume);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("click", handlePlayAndStop);
+video.addEventListener("timeupdate", handleTimeUpdate);
+timeline.addEventListener("input", handleTimelineChange);
+fullScreenBtn.addEventListener("click", handleFullScreen);
+document.addEventListener("keypress", handleShortCut);
+
+console.log("Hit!!");
